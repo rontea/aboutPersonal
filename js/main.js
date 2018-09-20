a92a8e2a02aee52fee1ef9c41efad40f7865ead2
Rontea <ronryanteano@gmail.com>
2018-09-20 14:58:14 -0400

    Add JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-// JavaScript Document
+/*
+	Graph
+*/
+
+
+var item = $("#skill1");
+
+
+var mySkillChart = new Chart(item, {
+    type: 'doughnut',
+    data: data,
+   
+});
+
+var data = {
+    datasets: [{
+        data: [10]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        
+    ]
+};
