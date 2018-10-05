9cc8779ffcdcca9e09aaec163333b566ed1effef
Rontea <ronryanteano@gmail.com>
2018-10-05 08:01:32 -0400

    Update Chart

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
 
 	
-	var ctx = $("#skills");
-
-	var myChart = new Chart(ctx, {
+	var skill1 = $("#skill1");
+	var skill2 = $("#skill2");
+	
+	var option = {
+		
+		display: false,
+		legend: {
+            display: false,
+			position: 'left',
+         },
+		tooltips: {
+            enabled: false
+         },
+		maintainAspectRatio: false,
+ 
+    };
+	
+	
+	
+	
+	var myChart = new Chart(skill1, {
     type: 'doughnut',
     data: {
         labels: ["Red"],
@@ -22,19 +40,77 @@ $(document).ready(function () {
             borderWidth: 1
         }]
     },
-    options: {
-		
-		display: false,
-		legend: {
-            display: false
-         },
-		tooltips: {
-            enabled: false
-         },
-        
-    }
+   options: option,
 });
 	
+var myChart = new Chart(skill2, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red"],
+        datasets: [{
+            
+            data: [60,40],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                
+            ],
+            borderWidth: 1
+        }]
+    },
+   options: option,
+});
+	
+
+var myChart = new Chart(skill3, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red"],
+        datasets: [{
+            
+            data: [60,40],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                
+            ],
+            borderWidth: 1
+        }]
+    },
+   options: option,
+});
+	
+
+var myChart = new Chart(skill4, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red"],
+        datasets: [{
+            
+            data: [60,40],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                
+            ],
+            borderWidth: 1
+        }]
+    },
+   options: option,
+});
+
 	
 	
 });
